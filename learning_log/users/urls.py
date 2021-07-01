53dec53388fc9a6a5dd4c018c8ab98e8376cd590
SaeranRao <960569261@qq.com>
2021-07-01 23:12:52 +0800

    解决了Base中Users中相关链接未添加{}导致的跳转出错问题，现在已可以正常登录、注销、注册

--- learning_log/users/urls.py
+++ learning_log/users/urls.py
@@ -1,8 +1,11 @@
 """为应用程序users的URL模式。"""
 from django.urls import path,include
+from . import views
 
 app_name = 'users'
 urlpatterns = [
     #默认的身份验证URL
     path('',include('django.contrib.auth.urls')),
+    #注册页面
+    path('register/',views.register,name = 'register'),
 ]
